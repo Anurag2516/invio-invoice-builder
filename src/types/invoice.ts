d2b14bd27576cc59d859bede9198c5dd9f9f9cc5
Anurag <anuragyadav2516@gmail.com>
2026-04-01 00:23:35 +0530

    feat(types): define types for invoice

--- src/types/invoice.ts
+++ src/types/invoice.ts
@@ -1,1 +1,31 @@
+export interface Invoice {
+  id: string;
+  invoiceNumber: string;
+  sender: User;
+  client: User;
+  issueDate: string;
+  dueDate: string;
+  items: lineItems[];
+  currency: string;
+  subtotal: number;
+  tax: number;
+  discount: number;
+  total: number;
+  notes: string;
+}
 
+export interface User {
+  companyName: string;
+  address: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+export interface lineItems {
+  serialNumber: number;
+  description: string;
+  quantity: number;
+  rate: number;
+  amount: number;
+}
