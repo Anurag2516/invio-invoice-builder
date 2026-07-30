5072e4a4db630c1fb6c361a1d2192092ab0fc63f
Anurag <anuragyadav2516@gmail.com>
2026-07-30 18:42:56 +0530

    feat: set up authentication flow with protected routes and React Query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,15 @@ import InvoicePreviewModal from "./components/invoice-preview/InvoicePreviewModa
 import { useEffect } from "react";
 import { useAppStore } from "./store/appStore";
 import { TooltipProvider } from "./components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useAuthStore } from "./store/authStore";
+import ProtectedRoutes from "./components/layout/ProtectedRoutes";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { AuthSkeleton } from "./components/ui/skeleton/AuthSkeleton";
+
+const queryClient = new QueryClient();
 
 function AppRoutes() {
   const location = useLocation();
@@ -26,10 +35,18 @@ function AppRoutes() {
   return (
     <TooltipProvider>
       <Routes location={backgroundLocation || location}>
-        <Route path="/" element={<Home />} />
-        <Route path="/invoices/new" element={<Invoice />} />
-        <Route path="/invoices/:id/edit" element={<Invoice />} />
-        <Route path="/invoices/:id/preview" element={<InvoicePreviewModal />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/invoices/new" element={<Invoice />} />
+          <Route path="/invoices/:id/edit" element={<Invoice />} />
+          <Route
+            path="/invoices/:id/preview"
+            element={<InvoicePreviewModal />}
+          />
+        </Route>
+
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
 
       {backgroundLocation && (
@@ -45,10 +62,20 @@ function AppRoutes() {
 }
 
 function App() {
+  const { initializeAuth, isAuthLoading } = useAuthStore();
+
+  useEffect(() => {
+    initializeAuth();
+  }, []);
+
+  if (isAuthLoading) return <AuthSkeleton />;
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
