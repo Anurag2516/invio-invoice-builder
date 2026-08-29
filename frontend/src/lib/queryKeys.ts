09442a1643aaeaa5b59617d3d10b4e3cb02a022f
Anurag <anuragyadav2516@gmail.com>
2026-08-29 20:47:21 +0530

    feat: add user and client keys

--- frontend/src/lib/queryKeys.ts
+++ frontend/src/lib/queryKeys.ts
@@ -1,4 +1,14 @@
 export const invoiceKey = {
-    all: ["invoices"] as const,
-    single: (id: string) => ["invoices", id] as const
-} 
+  all: ["invoices"] as const,
+  single: (id: string) => ["invoices", id] as const,
+  nextInvoiceNumber: ["nextInvoiceNumber"],
+};
+
+export const userKey = {
+  userProfile: ["user-profile"] as const
+};
+
+export const clientKey = {
+  all: ["clients"] as const,
+  single: (id: string) => ["clients", id] as const
+}
