92f5479ba15cff51c313970483c3b68db1e00b56
Anurag <anuragyadav2516@gmail.com>
2026-08-29 20:36:22 +0530

    refactor: update invoice API for drafts, autosave, and response types

--- frontend/src/api/invoice.api.ts
+++ frontend/src/api/invoice.api.ts
@@ -1,32 +1,59 @@
 import api from "@/lib/axios";
-import type { Invoice } from "@/types/invoice";
+import type {
+  DraftInvoiceResponse,
+  InvoiceFormData,
+  InvoiceResponse,
+  LineItemResponse,
+} from "@/types/invoice";
+import { formDataToApiPayload, formDataToAutoSavePayload } from "@/mapper/invoice.mapper";
 
-export const createInvoice = async (invoice: Invoice): Promise<Invoice> => {
-  const res = await api.post("api/invoice/createInvoice", invoice);
-
+export const createDraftInvoice = async (): Promise<DraftInvoiceResponse> => {
+  const res = await api.post("/api/invoice/createInvoice/draft");
   return res.data;
 };
 
-export const updateInvoice = async (id: string, invoice: Partial<Invoice>) => {
-  const res = await api.put(`api/invoice/updateInvoice/${id}`, invoice);
-
+export const autoSaveInvoice = async (
+  id: string,
+  invoice: InvoiceFormData,
+) => {
+  const invoicePayload = formDataToAutoSavePayload(invoice);
+  const res = await api.patch(`/api/invoice/autoSaveInvoice/${id}`, invoicePayload);
   return res.data;
 };
 
-export const getInvoice = async (id: string): Promise<Invoice> => {
-  const res = await api.get(`api/invoice/getInvoice/${id}`);
-
+export const updateInvoice = async (
+  id: string,
+  invoice: InvoiceFormData,
+): Promise<InvoiceResponse> => {
+  const invoicePayload = formDataToApiPayload(invoice);
+  const res = await api.put(`/api/invoice/updateInvoice/${id}`, invoicePayload);
   return res.data;
 };
 
-export const getInvoices = async (): Promise<Invoice[]> => {
-  const res = await api.get("api/invoice/getInvoices");
+export const getInvoice = async (id: string): Promise<InvoiceResponse> => {
+  const res = await api.get(`/api/invoice/getInvoice/${id}`);
+   return {
+     ...res.data,
+     subtotal: Number(res.data.subtotal),
+     taxRate: Number(res.data.taxRate),
+     taxAmount: Number(res.data.taxAmount),
+     discountRate: Number(res.data.discountRate),
+     discountAmount: Number(res.data.discountAmount),
+     total: Number(res.data.total),
+     lineItems: res.data.lineItems.map((item: LineItemResponse) => ({
+       quantity: Number(item.quantity),
+       rate: Number(item.rate),
+       amount: Number(item.amount),
+     })),
+   };
+};
 
+export const getInvoices = async (): Promise<InvoiceResponse[]> => {
+  const res = await api.get("/api/invoice/getInvoices");
   return res.data;
 };
 
 export const deleteInvoice = async (id: string) => {
-  const res = await api.delete(`api/invoice/deleteInvoice/${id}`);
-
+  const res = await api.delete(`/api/invoice/deleteInvoice/${id}`);
   return res.data;
 };
